9fbbee7fe0959379db8044e70a5dba291fefa077
David Knothe <dknothe314@me.com>
2021-05-28 15:11:23 +0200

    Add a test

--- happy-rad/tests.hs
+++ happy-rad/tests.hs
@@ -6,10 +6,11 @@ import Data.List
 main = do
   dir <- getDataDir
   let workingTests = ["Test.ly", "TestMulti.ly", "TestPrecedence.ly", "bug001.ly", "precedence002.y", "bogus-token.y", "bug002.y", "Partial.ly", "issue91.y", "issue94.y"]
+  let customTests = ["EpsilonAnnounce.y"]
   let setup = TestSetup {
     happyExec = "happy-rad",
     defaultTests = workingTests,
-    customTests = [],
+    customTests = customTests,
     customDataDir = dir,
     allArguments = ["--rad", "--rad --types"],
     stopOnFailure = False
