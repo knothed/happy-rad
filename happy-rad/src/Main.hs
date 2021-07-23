4956a4ad19060950e2e70a24094261668f5d88b2
David Knothe <dknothe314@me.com>
2021-07-23 23:49:31 +0200

    Adapt to GLR-backend split-off

--- happy-rad/src/Main.hs
+++ happy-rad/src/Main.hs
@@ -3,55 +3,91 @@ module Main where
 import qualified Happy.Frontend.CLI as FrontendCLI
 import qualified Happy.Middleend.CLI as MiddleendCLI
 import qualified Happy.Backend.CLI as BackendCLI
-import qualified Happy.Backend.RAD.CLI as RADCLI
-import Happy.Backend
-import Happy.Backend.RAD
-import Happy.Backend.RAD.CLI
+import qualified Happy.Backend.RAD.CLI as RADBackendCLI
+import qualified Happy.Backend.GLR.CLI as GLRBackendCLI
+import qualified Happy.Backend as Backend
+import qualified Happy.Backend.RAD as RADBackend
+import qualified Happy.Backend.GLR as GLRBackend
+import Happy.Core.GenUtils
 import Happy.Core.OptionParsing
 import System.IO
-import System.Exit
 import System.Environment
 import System.Console.GetOpt
 import Control.Monad.Trans.Except
 import Control.Monad.Except
-import Paths_happy_rad
+import Paths_happy_rad (version)
 
--- Flag conglomerate
-data HappyFlag = Frontend FrontendCLI.Flag | Middleend MiddleendCLI.Flag | Backend BackendCLI.Flag | RAD RADCLI.Flag deriving Eq
+-- All flags
+data HappyFlag = Frontend FrontendCLI.Flag | Middleend MiddleendCLI.Flag | Backend BackendCLI.Flag | GLRBackend GLRBackendCLI.Flag | RADBackend RADBackendCLI.Flag deriving Eq
 
 as :: Functor f => [f a] -> (a -> b) -> [f b]
 a `as` b = map (fmap b) a
 
-allOptions :: [OptDescr HappyFlag]
-allOptions = FrontendCLI.options `as` Frontend  ++ MiddleendCLI.options `as` Middleend ++ BackendCLI.options `as` Backend ++ RADCLI.optionsWithoutOutfile `as` RAD
-
 getFrontend :: [HappyFlag] -> [FrontendCLI.Flag]
 getMiddleend :: [HappyFlag] -> [MiddleendCLI.Flag]
 getBackend :: [HappyFlag] -> [BackendCLI.Flag]
-getRad :: [HappyFlag] -> [RADCLI.Flag]
+getGLRBackend :: [HappyFlag] -> [GLRBackendCLI.Flag]
+getRADBackend :: [HappyFlag] -> [RADBackendCLI.Flag]
 getFrontend flags = [a | Frontend a <- flags]
 getMiddleend flags = [a | Middleend a <- flags]
 getBackend flags = [a | Backend a <- flags]
-getRad flags = [a | RAD a <- flags]
+getGLRBackend flags = [a | GLRBackend a <- flags]
+getRADBackend flags = [a | RADBackend a <- flags]
+
+-- Stick options togehter from all packages
+allOptions :: [OptDescr HappyFlag]
+allOptions =
+  FrontendCLI.options `as` Frontend ++
+  MiddleendCLI.options `as` Middleend ++
+  BackendCLI.options `as` Backend ++
+  -- Add the "--glr" option. Remove options that are already declared in happy-backend like outfile, template, ghc, debug.
+  removeAllOverlaps BackendCLI.options (GLRBackendCLI.characteristicOption : GLRBackendCLI.options) `as` GLRBackend ++
+  -- Add the "--rad" option. Remove options that are already declared in happy-backend like outfile.
+  -- Also remove possible short-option overlaps with GLR flags.
+  (cleanShortOverlaps GLRBackendCLI.options . removeAllOverlaps BackendCLI.options) (RADBackendCLI.characteristicOption : RADBackendCLI.options) `as` RADBackend
 
 -- Main
 main :: IO ()
 main = do
-  let sortedOpts = beginOptionsWith "oip" allOptions -- outfile, info, pretty
+  let sortedOpts = beginOptionsWith "oip" allOptions -- Order: outfile, info, pretty
   (flags, freeOpts) <- parseOptions sortedOpts version =<< getArgs
   filename <- requireUnnamedArgument freeOpts sortedOpts DieUsage0 DieUsageMult
-
   basename <- FrontendCLI.getBaseName filename
-  grammar <- try $ FrontendCLI.parseAndRun (getFrontend flags) filename basename
 
+  grammar <- try $ FrontendCLI.parseAndRun (getFrontend flags) filename basename
   (action, goto, items, unused_rules) <- MiddleendCLI.parseAndRun (getMiddleend flags) filename basename grammar
 
-  backend <- BackendCLI.parseFlags (getBackend flags) basename
-  let radFlags = getRad flags ++ [OptRAD_Outfile $ Happy.Backend.outFile backend] -- Pass outfile from backend to rad-backend
+  -- Backend / GLRBackend / RADBackend switching
+  let glrFlags = getGLRBackend flags
+  let radFlags = getRADBackend flags
+  let isGLR = GLRBackendCLI.hasCharacteristicFlag glrFlags
+  let isRAD = RADBackendCLI.hasCharacteristicFlag radFlags
+  backendOpts <- BackendCLI.parseFlags (getBackend flags) basename
 
-  case RADCLI.parseFlags radFlags basename of
-    Just rad -> runRADBackend rad grammar action goto items unused_rules
-    Nothing -> runBackend backend grammar action goto
+  case (isGLR, isRAD) of
+    (True, True) -> dieHappy "You cannot use GLR and RAD at the same time."
+    (True, False) -> GLRBackend.runGLRBackend (createGLROpts glrFlags backendOpts basename) grammar action goto
+    (False, True) -> RADBackend.runRADBackend (createRADOpts radFlags backendOpts basename) grammar action goto items unused_rules
+    (False, False) -> Backend.runBackend backendOpts grammar action goto
+
+-- Fill those glr-options that were removed due to overlap with happy-backend's options
+createGLROpts :: [GLRBackendCLI.Flag] -> Backend.BackendArgs -> String -> GLRBackend.GLRBackendArgs
+createGLROpts glrFlags backendOpts basename =
+  let glrOpts' = GLRBackendCLI.parseFlags glrFlags basename
+  in glrOpts' {
+    GLRBackend.outFile = Backend.outFile backendOpts,
+    GLRBackend.templateDir = Backend.templateDir backendOpts,
+    GLRBackend.ghc = Backend.ghc backendOpts,
+    GLRBackend.debug = Backend.debug backendOpts
+    }
+
+-- Fill those rad-options that were removed due to overlap with happy-backend's options
+createRADOpts :: [RADBackendCLI.Flag] -> Backend.BackendArgs -> String -> RADBackend.RADBackendArgs
+createRADOpts radFlags backendOpts basename =
+  let radOpts' = RADBackendCLI.parseFlags radFlags basename
+  in radOpts' {
+    RADBackend.outFile = Backend.outFile backendOpts
+    }
 
 try :: IO (Either String a) -> IO a
 try f = do
--- rad-backend/src/Happy/Backend/RAD/CLI.hs
+++ rad-backend/src/Happy/Backend/RAD/CLI.hs
@@ -1,4 +1,4 @@
-module Happy.Backend.RAD.CLI(Flag(..), options, optionsWithoutOutfile, hasRADFlag, parseFlags) where
+module Happy.Backend.RAD.CLI(Flag(..), options, characteristicOption, hasCharacteristicFlag, parseFlags) where
 
 import Happy.Backend.RAD
 import System.Console.GetOpt
@@ -7,58 +7,53 @@ import System.Console.GetOpt
 
 data Flag = 
     OptRAD
-  | OptRAD_Outfile String
-  | OptRAD_Comments
-  | OptRAD_ShowTypes
-  | OptRAD_RankNTypes
-  | OptRAD_ParserType String
+  | OptOutfile String
+  | OptComments
+  | OptShowTypes
+  | OptRankNTypes
+  | OptParserType String
   deriving Eq
 
+-- All options, without the characteristic "rad" option.
 options :: [OptDescr Flag]
 options = [
-    Option "r" ["rad"] (NoArg OptRAD) "generate a recursive ascent-descent parser",
-    Option "o" ["outfile"] (ReqArg OptRAD_Outfile "FILE") "write the output to FILE (default: file.hs)",
-    Option "" ["comments"] (NoArg OptRAD_Comments) "annotate each function with its items (rad-only)",
-    Option "" ["types"] (NoArg OptRAD_ShowTypes) "declare the type of all functions (rad-only)",
-    Option "" ["rank-n"] (NoArg OptRAD_RankNTypes) "use when some nonterminals have rank-n-types (n > 1) (rad-only)",
-    Option "" ["parsertype"] (ReqArg OptRAD_ParserType "NAME") "use NAME instead of `Parser` as the type for the parser wrapper. Only relevant in conjunction with --types."
+    Option "o" ["outfile"] (ReqArg OptOutfile "FILE") "write the output to FILE (default: file.hs)",
+    Option "c" ["comments"] (NoArg OptComments) "annotate each RAD function with its items",
+    Option "t" ["types"] (NoArg OptShowTypes) "declare the type of all RAD functions",
+    Option "n" ["rank-n"] (NoArg OptRankNTypes) "declare the explicit rank-n type of all RAD functions",
+    Option "" ["parsertype"] (ReqArg OptParserType "NAME") "use NAME instead of `Parser` as type for the RAD parser wrapper"
   ]
-    
--- Same as `options`, but missing the `outfile` option. Useful when using `rad-backend` together with happy's `backend` (which also provides an outfile option).
-optionsWithoutOutfile :: [OptDescr Flag]
-optionsWithoutOutfile = deleteAt 1 options where
-  deleteAt idx xs = lft ++ rgt
-    where (lft, (_:rgt)) = splitAt idx xs
+
+-- Use this option when using this backend together with another backend.
+characteristicOption :: OptDescr Flag
+characteristicOption = Option "r" ["rad"] (NoArg OptRAD) "generate a recursive ascent-descent parser"
 
 -------- [Flag] to RADBackendArgs conversion --------
 
--- If the OptRAD flag is not set, return Nothing.
-parseFlags :: [Flag] -> String -> Maybe RADBackendArgs
-parseFlags cli base = if not (hasRADFlag cli) then Nothing else Just RADBackendArgs {
+-- Determines whether the characteristic "rad" flag is set.
+hasCharacteristicFlag :: [Flag] -> Bool
+hasCharacteristicFlag = elem OptRAD
+
+parseFlags :: [Flag] -> String -> RADBackendArgs
+parseFlags cli base = RADBackendArgs {
   outFile = getOutputFileName base cli,
   typeAnnotations = getTypeAnnotations cli,
-  showComments = getComments cli,
+  showComments = OptComments `elem` cli,
   parserWrapperType = getParserType cli
 }
 
-hasRADFlag :: [Flag] -> Bool
-hasRADFlag = elem OptRAD
-
 getTypeAnnotations :: [Flag] -> TypeAnnotations
 getTypeAnnotations cli
-  | elem OptRAD_RankNTypes cli = RankN
-  | elem OptRAD_ShowTypes cli = Always
+  | elem OptRankNTypes cli = RankN
+  | elem OptShowTypes cli = Always
   | otherwise = Never
 
-getComments :: [Flag] -> Bool
-getComments = elem OptRAD_Comments
-
 getParserType :: [Flag] -> Maybe String
-getParserType cli = case [ s | (OptRAD_ParserType s) <- cli ] of
+getParserType cli = case [ s | (OptParserType s) <- cli ] of
   []    -> Nothing
   list  -> Just $ last list
 
 getOutputFileName :: String -> [Flag] -> String
-getOutputFileName base cli = case [ s | (OptRAD_Outfile s) <- cli ] of
+getOutputFileName base cli = case [ s | (OptOutfile s) <- cli ] of
   []    -> base ++ ".hs"
   list  -> last list
